cc3acd040e408d75cf2527bf663abf7c52b13bdc
Paranhas <rodrigoparanhas.br@gmail.com>
2025-05-27 14:08:25 -0300

    Aula 03 parte 2 de 2

--- Assets/Scripts/Menu/MenuButtonsManager.cs
+++ Assets/Scripts/Menu/MenuButtonsManager.cs
@@ -5,5 +5,32 @@ using DG.Tweening;
 
 public class MenuButtonsManager : MonoBehaviour
 {
-    
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private void OnEnable()
+    {
+        HideAllButtons();
+        ShowButtons();
+    }
+    private void HideAllButtons()
+    {
+        foreach(var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+        }
+    }
+    public void ShowButtons()
+    {
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var b  = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i * delay);
+        }
+    }
 }
