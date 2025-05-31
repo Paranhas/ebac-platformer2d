b574c3b15ba347a12fccbb5e39ad15689bcf7238
Paranhas <rodrigoparanhas.br@gmail.com>
2025-05-31 18:57:38 -0300

    Add Estado de vida e morte do player e um inimigo criado

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public int damage = 10;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        var health = collision.gameObject.GetComponent<HealthBase>();
+        if(health != null)
+        {
+            health.Damage(damage);
+        }
+    }
+}
