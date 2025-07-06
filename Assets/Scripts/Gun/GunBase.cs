c50f27e269a2a0889ffe1b3481c256423f56cb0b
Paranhas <rodrigoparanhas.br@gmail.com>
2025-07-06 15:18:02 -0300

    Add Aula 04 M16
    Feito a arma e o projétil do personagem, e também inserido vida ao enemy.

---
+++ Assets/Scripts/Gun/GunBase.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBase : MonoBehaviour
+{
+    public ProjectileBase prefabProjectile;
+    public Transform positionToShoot;
+    public float timeBetweenShoot = 0.1f;
+    private Coroutine _currentCoroutine;
+    public Transform playerSideReference;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _currentCoroutine = StartCoroutine(StartShoot());
+        }else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if(_currentCoroutine != null)
+            {
+                StopCoroutine(_currentCoroutine);
+            }
+        }
+    }
+
+   IEnumerator StartShoot()
+    {
+        while (true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(timeBetweenShoot);
+        }
+    }
+
+    public void Shoot()
+    {
+        var projectile = Instantiate(prefabProjectile);
+        projectile.transform.position = positionToShoot.position;
+        projectile.side = playerSideReference.transform.localScale.x;
+    }
+}
